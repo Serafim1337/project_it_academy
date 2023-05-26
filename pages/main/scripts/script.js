8fdc0f9550384a27a50fa7a6813ed4d40e24d3da
Serafim <7620226@gmail.com>
2023-05-26 23:27:22 +0300

    feat: made a structure of main page

--- pages/main/scripts/script.js
+++ pages/main/scripts/script.js
@@ -15,41 +15,65 @@
 // }
 // })();
 
-const container = document.getElementsByClassName('letters-container')[0];
+//! falling letters
+
+const container = document.getElementsByClassName("letters-container")[0];
 
 function randLetters() {
-    const spawnLettersLimit = 40;
-    const widthAdjust = 85;
-    
-    var alphabet = "abcdefghijklmnopqrstuvwxyz";
-    
-    var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-    
-    var posX = randLoc();
+  const spawnLettersLimit = 40;
+  const widthAdjust = 85;
 
-    let span = document.createElement('span');
-    span.textContent = randomLetter;
-    span.classList.add('falling-letter')
-    span.style.left = posX + 'px';
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    container.append(span);
-    
+  var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-    if(container.children.length > spawnLettersLimit) {
-        container.firstChild.remove();
-    }
+  var posX = randLoc();
 
-    
-    function randLoc() {
-      var randomLocation = Math.floor(Math.random() * window.innerWidth);
-      if(randomLocation > window.innerWidth - widthAdjust) {
-        return window.innerWidth - widthAdjust;
-      } else {
-        return randomLocation;
-      }
+  let span = document.createElement("span");
+  span.textContent = randomLetter;
+  span.classList.add("falling-letter");
+  span.style.left = posX + "px";
+
+  container.append(span);
+
+  if (container.children.length > spawnLettersLimit) {
+    container.firstChild.remove();
+  }
+
+  function randLoc() {
+    var randomLocation = Math.floor(Math.random() * window.innerWidth);
+    if (randomLocation > window.innerWidth - widthAdjust) {
+      return window.innerWidth - widthAdjust;
+    } else {
+      return randomLocation;
     }
   }
-  
-  setInterval(function() {
-    randLetters();
-  }, 200);
+}
+
+setInterval(function () {
+  randLetters();
+}, 200);
+
+//! block desc
+const menuContainer = document.querySelector(".menu-container");
+const wordsBlock = document.querySelector(".words-block");
+const pageBlock = document.querySelector(".page-block");
+
+const wordsDesc = document.createElement("div");
+wordsDesc.classList.add("block-description");
+wordsDesc.style.width = wordsBlock.offsetWidth + "px";
+wordsDesc.style.height = wordsBlock.offsetHeight + "px";
+wordsDesc.style.top = wordsBlock.offsetTop + "px";
+wordsDesc.style.left = wordsBlock.offsetLeft + "px";
+
+menuContainer.append(wordsDesc);
+
+wordsDesc.addEventListener("mouseenter", blockDescHandler);
+
+function blockDescHandler(e) {
+  console.log("mouse");
+  console.log(e.target);
+  e.target.style.top = 600 + "px";
+}
+
+const pageDesc = document.createElement("div");
